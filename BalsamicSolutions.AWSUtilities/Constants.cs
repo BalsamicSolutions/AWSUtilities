b8751d442703dcb90ca55ba5c192af5cfe51a56c
Robert Ginsburg <robert.ginsburg@balsamicsolutions.com>
2019-12-18 06:30:59 -0500

    initial project scaffolding

---
+++ BalsamicSolutions.AWSUtilities/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BalsamicSolutions.AWSUtilities
+{
+    /// <summary>
+    /// constants and shared statics
+    /// </summary>
+    public class Constants
+    {
+    }
+}
