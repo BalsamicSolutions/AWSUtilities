d84338ad7d1a0edf7606ebba837fee865bca43c9
Robert Ginsburg <robert.ginsburg@balsamicsolutions.com>
2020-03-16 09:56:40 -0400

    documentation update

--- ConsoleCore.Demos/Program.cs
+++ ConsoleCore.Demos/Program.cs
@@ -17,6 +17,7 @@ using Microsoft.Extensions.Logging;
 using Demo.Data;
 using Demo.Data.Testing;
 using BalsamicSolutions.AWSUtilities.Extensions;
+using BalsamicSolutions.AWSUtilities.SQS;
 
 namespace ConsoleCore.Demos
 {
@@ -24,7 +25,8 @@ namespace ConsoleCore.Demos
     {
 
         /// <summary>
-        /// refer to this link for how to operate a hosted sercvie https://docs.microsoft.com/en-us/aspnet/core/fundamentals/host/generic-host?view=aspnetcore-2.2
+        /// refer to this link for how to operate a hosted sercvie 
+        /// https://docs.microsoft.com/en-us/aspnet/core/fundamentals/host/generic-host?view=aspnetcore-2.2
         /// main entry point for .net core queue engine, almost all of the  RX
         /// engines will be similar to this one
         /// </summary>
@@ -34,6 +36,7 @@ namespace ConsoleCore.Demos
         {
             bool efDemo = false;
             bool sqsDemo = false;
+            bool sqsEnqueue = false;
 
             if (null != args && args.Length >= 0)
             {
@@ -41,21 +44,21 @@ namespace ConsoleCore.Demos
                 {
                     string arg = args[argIdx].ToLowerInvariant().Trim(new char[] { '/', '-', ' ' });
                     if (arg == "sqs") sqsDemo = true;
+                    if (arg == "sqsenqueue") sqsEnqueue = true;
+                    if (arg == "enqueue") sqsEnqueue = true;
                     if (arg == "ef") sqsDemo = true;
-                    if (arg == "sqsDemo") sqsDemo = true;
-                    if (arg == "efDemo") efDemo = true;
+                    if (arg == "sqsdemo") sqsDemo = true;
+                    if (arg == "efdemo") efDemo = true;
                     if (arg == "ignoresslerrors")
                     {
                         System.Net.ServicePointManager.ServerCertificateValidationCallback = new System.Net.Security.RemoteCertificateValidationCallback(delegate { return true; });
                     }
                 }
             }
-            if (efDemo)
-            { 
-                Console.WriteLine("TODO build a menu to select ef initialization and/or query examples")                ;
-            }
-            else if (sqsDemo)
+
+            if (sqsDemo)
             {
+                Console.WriteLine("starting sqs queue service (ctrl-c to exit)");
                 string localPath = Directory.GetCurrentDirectory();
                 IHostBuilder hostbuilder = new HostBuilder()
                     .ConfigureAppConfiguration((builderContext, config) =>
@@ -85,14 +88,45 @@ namespace ConsoleCore.Demos
                     //stuff inbetween start/wait for shutdown
                     await svcHost.WaitForShutdownAsync();
                 }
+                Console.WriteLine("exiting sqs queue service");
             }
-            Console.WriteLine("Press X to exit...");
-            char exitChar = Console.ReadKey().KeyChar;
-            while (exitChar != 'X' && exitChar != 'x')
+            else
             {
-                exitChar = Console.ReadKey().KeyChar;
+                if (efDemo)
+                {
+                    Console.WriteLine("TODO build a menu to select ef initialization and/or query examples");
+                }
+                else if (sqsEnqueue)
+                {
+                    var config = GetConfiguration();
+                    string queueName = config.GetValue<string>("appSettings:QueueName");
+                    Console.WriteLine($"Queueing 20 messages to {queueName}");
+                    SqsQueueDispatcher<SQSDemoQueueData> sqsQueue = new SqsQueueDispatcher<SQSDemoQueueData>(queueName);
+                    for (int count = 0; count < 20; count++)
+                    {
+                        SQSDemoQueueData queueData = SQSDemoQueueData.RandomQueueData();
+                        sqsQueue.EnqueueMessage(queueData);
+                    }
+                    Console.WriteLine($"Completed queueing 20 messages to {queueName}");
+                }
+                Console.WriteLine("Press X to exit...");
+                char exitChar = Console.ReadKey().KeyChar;
+                while (exitChar != 'X' && exitChar != 'x')
+                {
+                    exitChar = Console.ReadKey().KeyChar;
+                }
             }
+
         }
 
+        //place holder to read configuration outside
+        //of the host builder
+        static IConfiguration GetConfiguration()
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+               .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false);
+            return builder.Build();
+        }
     }
 }
